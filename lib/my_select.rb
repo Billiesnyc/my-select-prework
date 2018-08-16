bc37813f7541847df80373060886f4dd6447e9a4
Billie Simmons <billiesnyc@gmail.com>
2018-08-16 12:56:58 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
     i = 0
     new_collection = []
     while i < collection.length
-      new_collection.push(if yield(collection[i]))
+      new_collection.push(yield(collection[i]))
       i += 1
     end
     new_collection
