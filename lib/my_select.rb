423072ff67a6e461e321e90648082d9b0116778e
Billie Simmons <billiesnyc@gmail.com>
2018-08-16 12:48:56 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,14 @@
 def my_select(collection)
-  
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+
+  else
+    puts "Hey! No block was given!"
+  end
   
 end
